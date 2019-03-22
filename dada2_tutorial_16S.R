dd8af96cb4a4d49533e23d62a171ecff2400cabd
Hannah Holland-Moritz <hhollandmoritz@gmail.com>
2019-03-22 16:21:54 -0600

    changed hollandh to YOUR_USERNAME
    add demultiplexed data
    
    deletes demultiplexeddata.tar.gz

--- dada2_tutorial_16S.R
+++ dada2_tutorial_16S.R
@@ -143,7 +143,7 @@ R2.fp <- file.path(data.fp, "Undetermined_S0_L001_R2_001.fastq.gz")
 #' you do not need to create the subdirectories but they are nice to have
 #' for organizational purposes. 
 
-project.fp <- "/data/hollandh/MicroMethods_dada2_tutorial" # CHANGE ME to project directory; don't append with a "/"
+project.fp <- "/data/YOUR_USERNAME/MicroMethods_dada2_tutorial" # CHANGE ME to project directory; don't append with a "/"
 
 # Set up names of sub directories to stay organized
 preprocess.fp <- file.path(project.fp, "01_preprocess")
